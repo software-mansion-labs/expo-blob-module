188cf63ba880cf9b58106a83574ace9ad76c24c6
Artur Gęsiarz <artur.gesiarz@swmansion.com>
2025-06-11 12:56:32 +0200

    clean-up

--- blob-module/src/BlobModule.types.ts
+++ blob-module/src/BlobModule.types.ts
@@ -1,19 +1,7 @@
-import type { StyleProp, ViewStyle } from 'react-native';
-
-export type OnLoadEventPayload = {
-  url: string;
-};
-
 export type BlobModuleEvents = {
-  onChange: (params: ChangeEventPayload) => void;
+	onChange: (params: ChangeEventPayload) => void;
 };
 
 export type ChangeEventPayload = {
-  value: string;
-};
-
-export type BlobModuleViewProps = {
-  url: string;
-  onLoad: (event: { nativeEvent: OnLoadEventPayload }) => void;
-  style?: StyleProp<ViewStyle>;
+	value: string;
 };
--- blob-module/src/index.ts
+++ blob-module/src/index.ts
@@ -1,5 +1,2 @@
-// Reexport the native module. On web, it will be resolved to BlobModule.web.ts
-// and on native platforms to BlobModule.ts
-export { default } from './BlobModule';
-export { default as BlobModuleView } from './BlobModuleView';
-export * from  './BlobModule.types';
+export { default } from "./BlobModule";
+export * from "./BlobModule.types";
