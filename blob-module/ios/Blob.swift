04615eeb491a06d9ce78a0694fd0c2b01d5a61fe
Artur Gęsiarz <artur.gesiarz@swmansion.com>
2025-07-15 13:58:41 +0200

    Tabs were deleted

--- blob-module/ios/Blob.swift
+++ blob-module/ios/Blob.swift
@@ -2,85 +2,87 @@ import Foundation
 import ExpoModulesCore
 
 public class Blob: SharedObject, BlobPart {
-    var blobParts: [BlobPart]
-    var options: BlobOptions
+  var blobParts: [BlobPart]
+  var options: BlobOptions
+  
+  init(blobParts: [BlobPart]?, options: BlobOptions?) {
+    self.blobParts = blobParts ?? []
+    self.options = options ?? BlobOptions()
+  }
+
+  var size: Int {
+    return blobParts.reduce(0) { $0 + $1.size }
+  }
+
+  var type: String {
+    return options.type
+  }
+
+  func slice(start: Int = 0, end: Int? = nil, contentType: String = "") -> Blob {
+    let startIdx = max(start, 0)
+    var endIdx = min(end ?? self.size - 1, self.size - 1)
+    let options = BlobOptions(type: contentType != "" ? contentType : self.options.type, endings: self.options.endings)
     
-    init(blobParts: [BlobPart]?, options: BlobOptions?) {
-        self.blobParts = blobParts ?? []
-        self.options = options ?? BlobOptions()
+    if endIdx < 0 {
+      endIdx = self.size - 1 + endIdx;
     }
 
-    var size: Int {
-        return blobParts.reduce(0) { $0 + $1.size }
-    }
-
-    var type: String {
-        return options.type
-    }
-
-    func slice(start: Int = 0, end: Int? = nil, contentType: String = "") -> Blob {
-        let startIdx = max(start, 0)
-        var endIdx = min(end ?? self.size - 1, self.size - 1)
-        let options = BlobOptions(type: contentType != "" ? contentType : self.options.type, endings: self.options.endings)
-        
-        if endIdx < 0 {
-            endIdx = self.size - 1 + endIdx;
-        }
-
-        if startIdx > endIdx || start == 0 && end == self.size - 1 {
-            return self
-        }
-        
-        var dataSlice: [BlobPart] = []
-        var charsLeft = endIdx - startIdx + 1
-        for part in blobParts {
-            if part.text().count <= charsLeft {
-                charsLeft -= part.text().count
-                dataSlice.append(part)
-            } else if charsLeft > 0 {
-                var partToAdd = ""
-                for char in part.text() {
-                    if charsLeft > 0 {
-                        charsLeft -= 1
-                        partToAdd += String(char)
-                    }
-                }
-                dataSlice.append(partToAdd)
-            } else {
-                break
-            }
-        }
-
-        return Blob(blobParts: dataSlice, options: options)
+    if startIdx > endIdx || start == 0 && end == self.size - 1 {
+      return self
     }
     
-    func text() -> String {
-        return blobParts.reduce("") { $0 + $1.text() }
+    var dataSlice: [BlobPart] = []
+    var charsLeft = endIdx - startIdx + 1
+    
+    for part in blobParts {
+      if part.text().count <= charsLeft {
+        charsLeft -= part.text().count
+        dataSlice.append(part)
+        
+      } else if charsLeft > 0 {
+        var partToAdd = ""
+        for char in part.text() {
+          if charsLeft > 0 {
+            charsLeft -= 1
+            partToAdd += String(char)
+          }
+        }
+        dataSlice.append(partToAdd)
+      } else {
+          break
+      }
     }
+
+    return Blob(blobParts: dataSlice, options: options)
+  }
+  
+  func text() -> String {
+    return blobParts.reduce("") { $0 + $1.text() }
+  }
 }
 
 enum EndingType: String, Enumerable {
-    case transparent = "transparent"
-    case native = "native"
+  case transparent = "transparent"
+  case native = "native"
 }
 
 struct BlobOptions: Record {
-    @Field
-    var type: String = ""
-    @Field
-    var endings: EndingType = .transparent
+  @Field
+  var type: String = ""
+  @Field
+  var endings: EndingType = .transparent
 }
 
 protocol BlobPart {
-    var size: Int { get }
-    func text() -> String
+  var size: Int { get }
+  func text() -> String
 }
 
 extension String: BlobPart {
-    var size: Int {
-        return self.count
-    }
-    func text() -> String {
-        return self
-    }
+  var size: Int {
+    return self.count
+  }
+  func text() -> String {
+    return self
+  }
 }
--- blob-module/ios/BlobModule.swift
+++ blob-module/ios/BlobModule.swift
@@ -2,39 +2,39 @@ import Foundation
 import ExpoModulesCore
 
 public class BlobModule: Module {
-    public func definition() -> ModuleDefinition {
-        Name("ExpoBlob")
-        
-        Class(Blob.self) {
-            Constructor { (blobParts: Either<[String], [Blob]>, options: BlobOptions?) in
-                if let blobPartsList: [String] = blobParts.get() {
-                    return Blob(blobParts: blobPartsList, options: options ?? BlobOptions())
-                }
-                else if let blobPartsSingleBlob: [Blob] = blobParts.get() {
-                    return Blob(blobParts: blobPartsSingleBlob, options: options ?? BlobOptions())
-                }
-                return Blob(blobParts: [], options: options ?? BlobOptions())
-            }
-            
-            Property("size") { (blob: Blob) in
-                blob.size
-            }
-            
-            Property("type") { (blob: Blob) in
-                blob.type
-            }
-            
-            Property("endings") { (blob: Blob) in
-                blob.options.endings
-            }
-            
-            Function("slice") { (blob: Blob, start: Int?, end: Int?, contentType: String?) in
-                blob.slice(start: start ?? 0, end: end ?? blob.size - 1, contentType: contentType ?? "")
-            }
-                
-            Function("text") { (blob: Blob) in
-                blob.text()
-            }
+  public func definition() -> ModuleDefinition {
+    Name("ExpoBlob")
+
+    Class(Blob.self) {
+      Constructor { (blobParts: Either<[String], [Blob]>, options: BlobOptions?) in
+        if let blobPartsList: [String] = blobParts.get() {
+          return Blob(blobParts: blobPartsList, options: options ?? BlobOptions())
         }
+        else if let blobPartsSingleBlob: [Blob] = blobParts.get() {
+          return Blob(blobParts: blobPartsSingleBlob, options: options ?? BlobOptions())
+        }
+        return Blob(blobParts: [], options: options ?? BlobOptions())
+      }
+      
+      Property("size") { (blob: Blob) in
+        blob.size
+      }
+      
+      Property("type") { (blob: Blob) in
+        blob.type
+      }
+      
+      Property("endings") { (blob: Blob) in
+        blob.options.endings
+      }
+      
+      Function("slice") { (blob: Blob, start: Int?, end: Int?, contentType: String?) in
+        blob.slice(start: start ?? 0, end: end ?? blob.size - 1, contentType: contentType ?? "")
+      }
+      
+      Function("text") { (blob: Blob) in
+        blob.text()
+      }
     }
+  }
 }
